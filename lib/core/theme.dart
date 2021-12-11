ba68d655eafc54c9c98d80c6038c449ab2925fc3
u.rakhmatullaev <u.rakhmatullayev@uic.group>
2021-12-11 12:57:08 +0100

    feat: add search textfield ui

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 abstract class AppTheme {
   static ThemeData lightTheme() => ThemeData(
-        appBarTheme: const AppBarTheme(color: surfaceColor),
+        appBarTheme: const AppBarTheme(
+          color: surfaceColor,
+        ),
         fontFamily: 'SF-Pro-Display',
         scaffoldBackgroundColor: backgroundColor,
         canvasColor: backgroundColor,
         splashColor: Colors.transparent,
-        textTheme: const TextTheme(),
+        textTheme: const TextTheme(
+          bodyText1: bodyText1,
+          bodyText2: bodyText2,
+        ),
         backgroundColor: backgroundColor,
         colorScheme: const ColorScheme(
           surface: surfaceColor,
@@ -30,11 +36,19 @@ abstract class AppTheme {
   static const backgroundColor = Colors.black;
   static const surfaceColor = Color(0xff1B1F23);
   static const errorColor = Colors.red;
-
+  static const textGreyLight = Color(0xff6A737D);
+  static const primaryTextColor = Color(0xff0366D6);
   static const bodyText1 = TextStyle(
     fontFamily: 'SF-Pro-Display',
-    fontSize: 14,
+    fontSize: 18,
     fontWeight: FontWeight.normal,
-    color: Color(0xff6A737D),
+    color: textGreyLight,
+  );
+
+  static const bodyText2 = TextStyle(
+    fontFamily: 'SF-Pro-Display',
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:github_repos/core/constants.dart';
 import 'package:github_repos/core/theme.dart';
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       title: 'GitHub Repos',
       home: const RepositoriesScreen(),
       themeMode: ThemeMode.light,
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme(),
     );
   }
@@ -26,11 +28,13 @@ class RepositoriesScreen extends StatelessWidget {
   const RepositoriesScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: SearchAppbar(),
-    );
-  }
+  Widget build(BuildContext context) =>
+      const AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Scaffold(
+          appBar: SearchAppbar(),
+        ),
+      );
 }
 
 class SearchAppbar extends StatelessWidget with PreferredSizeWidget {
@@ -41,7 +45,15 @@ class SearchAppbar extends StatelessWidget with PreferredSizeWidget {
     return Container(
       height: 100,
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-      color: Theme.of(context).colorScheme.surface,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        border: const Border(
+          bottom: BorderSide(
+            color: Color(0xffD1D5DA),
+            width: 0.12,
+          ),
+        ),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
@@ -49,20 +61,26 @@ class SearchAppbar extends StatelessWidget with PreferredSizeWidget {
             height: 36,
             child: CupertinoTextField(
               keyboardType: TextInputType.visiblePassword,
-              style: Theme.of(context).textTheme.headline1!.copyWith(
-                    fontSize: 14,
-                  ),
+              cursorColor: AppTheme.primaryTextColor,
+              cursorWidth: 2,
+              cursorHeight: 20,
+              style: Theme.of(context).textTheme.bodyText2,
               placeholder: 'Search GitHub',
               placeholderStyle: Theme.of(context).textTheme.bodyText1,
               prefix: Row(
                 children: [
                   const SizedBox(width: 8),
-                  SvgPicture.asset(AppConstants.searchIcon),
+                  SvgPicture.asset(
+                    AppConstants.searchIcon,
+                    width: 24,
+                    height: 24,
+                    color: AppTheme.textGreyLight,
+                  ),
                 ],
               ),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
-                color: const Color(0xff444D56),
+                color: const Color(0xff2F363D),
               ),
             ),
           ),
@@ -72,5 +90,5 @@ class SearchAppbar extends StatelessWidget with PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(100);
+  Size get preferredSize => const Size.fromHeight(100);
 }
