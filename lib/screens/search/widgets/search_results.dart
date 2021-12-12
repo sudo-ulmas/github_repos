3e1f958a17644f3fc25b236484b4c604f2a180c8
u.rakhmatullaev <u.rakhmatullayev@uic.group>
2021-12-12 19:24:54 +0100

    feat: add hover animation to search items

--- lib/screens/search/widgets/search_results.dart
+++ lib/screens/search/widgets/search_results.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:github_repos/core/constants.dart';
 import 'package:github_repos/models/repository.dart';
 import 'package:github_repos/screens/search/widgets/searched_item.dart';
 
@@ -11,15 +9,17 @@ class SearchResults extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
         color: Theme.of(context).colorScheme.surface,
-        child: ListView.separated(
-          separatorBuilder: (context, index) => const Divider(
-            thickness: 0.2,
-            height: 1,
-            indent: 20,
-            color: Color(0xffE1E4E8),
+        child: Scrollbar(
+          child: ListView.separated(
+            separatorBuilder: (context, index) => Divider(
+              thickness: 0.4,
+              height: 1,
+              indent: 20,
+              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+            ),
+            itemCount: repos.length,
+            itemBuilder: (context, index) => Searcheditem(repo: repos[index]),
           ),
-          itemCount: repos.length,
-          itemBuilder: (context, index) => Searcheditem(repo: repos[index]),
         ),
       );
 }
--- lib/screens/search/widgets/searched_item.dart
+++ lib/screens/search/widgets/searched_item.dart
@@ -3,72 +3,109 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:github_repos/core/constants.dart';
 import 'package:github_repos/models/repository.dart';
 
-class Searcheditem extends StatelessWidget {
+class Searcheditem extends StatefulWidget {
   final Repository repo;
   const Searcheditem({Key? key, required this.repo}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        padding: const EdgeInsets.all(20),
-        color: Theme.of(context).colorScheme.surface,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Image.network(
-                  repo.owner.avatarUrl,
-                  width: 24,
-                  height: 24,
-                ),
-                const SizedBox(width: 8),
-                Text(repo.owner.login,
-                    style: Theme.of(context).textTheme.bodyText1),
-              ],
-            ),
-            const SizedBox(height: 12),
-            Text(
-              repo.name,
-              style: Theme.of(context).textTheme.headline3,
-            ),
-            const SizedBox(height: 12),
-            if (repo.description != '') ...{
-              Text(
-                repo.description,
-                style: Theme.of(context).textTheme.headline3!.copyWith(
-                      fontSize: 18,
-                    ),
-              ),
-              const SizedBox(height: 12),
-            },
-            Row(
-              children: [
-                SvgPicture.asset(
-                  AppConstants.starIcon,
-                  color: Theme.of(context).colorScheme.onSurface,
-                ),
-                const SizedBox(width: 8),
-                Text(
-                  repo.starCount.toString(),
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                const SizedBox(width: 16),
-                Container(
-                  width: 12,
-                  height: 12,
-                  decoration: const BoxDecoration(
-                    color: Color(0xff34D058),
-                    shape: BoxShape.circle,
+  State<Searcheditem> createState() => _SearcheditemState();
+}
+
+class _SearcheditemState extends State<Searcheditem> {
+  bool pressed = false;
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+        onPanDown: (a) => setState(() {
+          pressed = true;
+        }),
+        onPanEnd: (a) => setState(() {
+          pressed = false;
+        }),
+        onPanCancel: () => setState(() {
+          pressed = false;
+        }),
+        child: Container(
+          color: Theme.of(context).colorScheme.surface,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 100),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: pressed
+                        ? Theme.of(context)
+                            .colorScheme
+                            .onSurface
+                            .withOpacity(0.5)
+                        : Colors.transparent,
                   ),
                 ),
-                const SizedBox(width: 8),
-                Text(
-                  repo.language,
-                  style: Theme.of(context).textTheme.headline5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Image.network(
+                          widget.repo.owner.avatarUrl,
+                          width: 24,
+                          height: 24,
+                        ),
+                        const SizedBox(width: 8),
+                        Text(widget.repo.owner.login,
+                            style: Theme.of(context).textTheme.bodyText1),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      widget.repo.name,
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                    const SizedBox(height: 12),
+                    if (widget.repo.description != '') ...{
+                      Text(
+                        widget.repo.description,
+                        style: Theme.of(context).textTheme.headline3!.copyWith(
+                              fontSize: 18,
+                            ),
+                      ),
+                      const SizedBox(height: 12),
+                    },
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          AppConstants.starIcon,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          widget.repo.starCount.toString(),
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                        const SizedBox(width: 16),
+                        Container(
+                          width: 12,
+                          height: 12,
+                          decoration: const BoxDecoration(
+                            color: Color(0xff34D058),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          widget.repo.language,
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       );
 }
